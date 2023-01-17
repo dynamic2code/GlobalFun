ffbce92ddb22f8de536ebc7e9e21531996e44c4e
Joseph Wakahu Ngacha <83699448+dynamic2code@users.noreply.github.com>
2023-01-17 10:14:20 +0300

    Addition to the final page and changes to button on click activity

--- app/src/main/java/com/example/globalfun/SignUp.kt
+++ app/src/main/java/com/example/globalfun/SignUp.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.NumberPicker.OnValueChangeListener
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.globalfun.ui.theme.GlobalFunTheme
@@ -54,6 +56,16 @@ fun top() {
 @Composable
 fun signUp(){
 
+    val textin = remember {
+        mutableStateOf(value = "")
+    }
+
+    TextField(
+        value = textin.value,
+        onValueChange = {
+            textin.value = it
+    }
+    )
 //    Box() {
 //        Column() {
 ////            the full box
