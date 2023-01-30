a5c7bee1af1466a65bdca583e3b39b9cf0ddd9df
Joseph Wakahu Ngacha <83699448+dynamic2code@users.noreply.github.com>
2023-01-30 12:20:50 +0300

    good search bar

--- app/src/main/java/com/example/globalfun/HomeActivity.kt
+++ app/src/main/java/com/example/globalfun/HomeActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -18,7 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.painterResource
+
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
